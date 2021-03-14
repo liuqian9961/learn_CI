da93aed01f9ad20238cb2db3f607e86b64d71363
LiuQian <liuqian9961@163.com>
2021-03-15 00:26:10 +0800

    test2

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import static org.junit.Assert.*;
 
 public class MainClassTest {
@@ -10,17 +10,5 @@ public class MainClassTest {
     @org.junit.Test
     public void main() {
     }
-=======
-import static org.junit.Assert.*;
 
-public class MainClassTest {
-
-    @org.junit.Test
-    public void init() {
-    }
-
-    @org.junit.Test
-    public void main() {
-    }
->>>>>>> 1e0423318553dc49f6ca51ec74532739015e2b17
 }
--- src/test/java/PolyTest.java
+++ src/test/java/PolyTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import static org.junit.Assert.*;
 
 public class PolyTest {
@@ -14,21 +14,4 @@ public class PolyTest {
     @org.junit.Test
     public void print() {
     }
-=======
-import static org.junit.Assert.*;
-
-public class PolyTest {
-
-    @org.junit.Test
-    public void getItem() {
-    }
-
-    @org.junit.Test
-    public void diffOfpoly() {
-    }
-
-    @org.junit.Test
-    public void print() {
-    }
->>>>>>> 1e0423318553dc49f6ca51ec74532739015e2b17
 }
